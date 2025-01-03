744966704a2c60c4a8c10bed530a56207988a0cd
a4z <harald.achitz@gmail.com>
2025-01-03 14:11:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Consumable XCFramework builds of zeromq/libzmq
+
+This is are build of [zeromq/libzmq](https://github.com/zeromq/libzmq) for
+
+- MacOS
+- iOS (and iOS Simulator)
+- watchOS (and watchOS Simulator)
+- tvOS (and tvOS Simulator)
+
+bundled as **XCFramework**.
+
+The builds can be downloaded from the [release section of this repo](https://github.com/a4z/libzmq-xcf/releases)
+An example application about how to consume this XCFramework via a Swift Package can be found in [the szq example repo](https://github.com/a4z/szq/).
+
+The build are minimal dependency builds.
+If you have a need for builds with some dependency, like libsodium, let me know. I might be able to help.
---
+++ create-release-tag.sh
@@ -0,0 +1,49 @@
+
+
+submodules_did_change() {
+    git submodule update --remote
+    SUBMODULE_CHANGES=$(git status --short --untracked-files=no)
+
+    if [ -n "$SUBMODULE_CHANGES" ]; then
+        echo "Update in submodules:"
+        echo "$SUBMODULE_CHANGES"
+        return 0
+    else
+        echo "No changes in the submodules."
+        return 1
+    fi
+}
+
+if submodules_did_change; then
+    echo "Submodules have changes."
+    git add -u .
+    git commit -m "Update submodules to the latest commit"
+    git push origin main
+else
+    echo "No new tag required."
+    exit 0
+fi
+
+REPO_URL="git@github.com:a4z/libzmq-xcf.git"
+git fetch --tags
+LATEST_TAG=$(git tag --sort=-v:refname | grep -E "^v4\.3\.5\-\d{6}_\d+$" | head -n 1)
+TODAY=$(date +%y%m%d)
+
+if [[ ${LATEST_TAG} =~ ^v4\.3\.5\-([0-9]{6})_([0-9]+)$ ]]; then
+    LATEST_DATE="${BASH_REMATCH[1]}"
+    LATEST_BUILD_NR="${BASH_REMATCH[2]}"
+    if [[ $LATEST_DATE == ${TODAY} ]]; then
+        BUILD_NR=$((LATEST_BUILD_NR + 1))
+    else
+        BUILD_NR=1
+    fi
+else
+    BUILD_NR=1
+fi
+
+NEW_TAG="v4.3.5-${TODAY}_${BUILD_NR}"
+echo "Create new tag: ${NEW_TAG}"
+git tag ${NEW_TAG}
+
+echo "TODO: Push the new tag to the remote repository. Run:"
+echo  git push origin ${NEW_TAG}
